cb2f73ee657e64b843e91c4da1720f9d79b82c7c
Jan Grünewald <jan.gruenewald84@googlemail.com>
2023-12-06 08:39:47 +0100

    completed day6

--- src/main/kotlin/rocks/gruenewald/aoc2023/Day06.kt
+++ src/main/kotlin/rocks/gruenewald/aoc2023/Day06.kt
@@ -3,27 +3,53 @@ package rocks.gruenewald.aoc2023
 import rocks.gruenewald.println
 import rocks.gruenewald.readInput
 import rocks.gruenewald.verifyTestResult
+import kotlin.math.pow
 
 fun main() {
+    fun Long.squared() = this.toDouble().pow(2).toLong()
 
-    fun part1(lines: List<String>): Any {
-        TODO("To be implemented.")
+    fun distanceForBoat(durationOfHold: Long, durationOfRace: Long): Long =
+        (durationOfRace * durationOfHold) - durationOfHold.squared()
+
+    fun calculatePositiveOutcomesPerRace(raceTime: Long, distanceToReach: Long): Long {
+        var positiveOutcomesPerRace = 0L
+        for (holdTime in 1 until raceTime) {
+            val distanceForBoat = distanceForBoat(holdTime, raceTime)
+            if (distanceForBoat > distanceToReach) {
+                positiveOutcomesPerRace = positiveOutcomesPerRace.inc()
+            }
+        }
+        return positiveOutcomesPerRace
     }
 
-    fun part2(lines: List<String>): Any {
-        TODO("To be implemented")
+    fun part1(lines: List<String>): Long {
+        val times = lines[0].split(' ').filter { it.toLongOrNull() != null }.map { it.toLong() }
+        val distances = lines[1].split(' ').filter { it.toLongOrNull() != null }.map { it.toLong() }
+
+        val allPositiveOutcomes = mutableListOf<Long>()
+        times.forEachIndexed { index, time ->
+            allPositiveOutcomes.add(calculatePositiveOutcomesPerRace(time, distances[index]))
+        }
+        return allPositiveOutcomes.reduce { acc, i -> acc * i }
     }
 
-    val part1TestInput = """        
+    fun part2(lines: List<String>): Long {
+        val time = lines[0].split(' ').filter { it.toLongOrNull() != null }.reduce { acc, s -> acc + s }.toLong()
+        val distance = lines[1].split(' ').filter { it.toLongOrNull() != null }.reduce { acc, s -> acc + s }.toLong()
+
+        return calculatePositiveOutcomesPerRace(time, distance)
+    }
+
+    val part1TestInput = """
+        Time:      7  15   30
+        Distance:  9  40  200
     """.trimIndent().split("\n")
-    verifyTestResult(part1(part1TestInput), Any())
+    verifyTestResult(part1(part1TestInput), 288L)
 
     val inputOfTheDay = readInput("day06")
     part1(inputOfTheDay).println()
 
-    val part2TestInput = """
-    """.trimIndent().split("\n")
-    verifyTestResult(part2(part2TestInput), Any())
+    verifyTestResult(part2(part1TestInput), 71503L)
     part2(inputOfTheDay).println()
 }
 
